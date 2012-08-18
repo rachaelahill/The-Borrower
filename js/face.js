38beaa9e3b2129575869e193f1d476863a5fa192
Rachael Hill <rachaelhill@Rachaels-MacBook-Pro.local>
2012-08-18 10:39:44 -0400

    updated version

--- js/face.js
+++ js/face.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-
 FB.init({
-    appId  : '275468342566427',
+    appId  : '456943801012883',
     status : true, // check login status
     cookie : true, // enable cookies to allow the server to access the session
     xfbml  : true, // parse XFBML
     oauth  : true // enable OAuth 2.0
   });
-
+  
 loginFace = function(){
 	if(hasRun){
 		return false;
@@ -15,7 +14,7 @@ loginFace = function(){
 		hasRun = true;
 	};
 	FB.api('/me', function(response) {
-  	
+
 		localStorage.setItem('fb_me', JSON.stringify(response));
 		console.log('**User Info stored locally', response);
 	});
@@ -25,7 +24,8 @@ loginFace = function(){
 	 	console.log('**Friends Info stored locally', response);
 	});
 	$.mobile.changePage('#explore');
-};
+	//$("#popList").listview('refresh');
+}
 
 	$(function(){
 		$('#getInfo').click(function() {
@@ -33,7 +33,7 @@ loginFace = function(){
 		});
 		
 		
-		$('#logoutFace').click(function() {
+		$('.btnLogout').click(function() {
 			FB.logout(function(response){
 				console.log('user logged out' , response);
 			});
@@ -41,26 +41,5 @@ loginFace = function(){
 			
 	});
 	var hasRun = false;
-	
-	/*
-FB.getLoginStatus(function(response) {
-  if (response.status === 'connected') {
-    // the user is logged in and has authenticated your
-    // app, and response.authResponse supplies
-    // the user's ID, a valid access token, a signed
-    // request, and the time the access token 
-    // and signed request each expire
-    $.mobile.changePage('#explore');
-   
-    var uid = response.authResponse.userID;
-    var accessToken = response.authResponse.accessToken;
-  } else if (response.status === 'not_authorized') {
-    // the user is logged in to Facebook, 
-    // but has not authenticated your app
-  } else {
-    // the user isn't logged in to Facebook.
-  }
- });
-*/
 });
 	
